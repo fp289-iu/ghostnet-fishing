c5ba37629533387eda136677da6c776a4888e150
ElCon <ElCon@10.2.0.2>
2026-04-04 22:15:15 +0200

    PersonDAO vergessene Annotation hinzugefügt

--- src/main/java/org/sheashepherd/ghostnet/dao/PersonDAO.java
+++ src/main/java/org/sheashepherd/ghostnet/dao/PersonDAO.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.sheashepherd.ghostnet.model.Person;
 import org.sheashepherd.ghostnet.model.Person.Typ;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 
+@ApplicationScoped
 public class PersonDAO {
 	
 	 @PersistenceContext(unitName = "ghostnetPU")
